2673a60145592e9923a642acaac777f68a8b39ab
Jimmy Béland-Bédard <jbeland@armrecouvrement.com>
2018-01-18 12:13:30 -0500

    Fix stuff

--- Modules/music.js
+++ Modules/music.js
@@ -1,8 +1,14 @@
 exports.commands = function (bot, modules, config, opus, ytdl, youtubeSearch, message) {
     const prefix = config.prefix;
-    const youtube = require('./youtube.js');
     var server = bot[message.guild.id];
     var args = message.content.split(' ');
+    var connection = null;
+    var dispatcher = null;
+    const streamOptions = {
+        seek: 0,
+        volume: 1,
+        bitrate: 96000
+    };
 
     if (message.content === (prefix + "list")) {
         if (server) {
@@ -59,13 +65,46 @@ exports.commands = function (bot, modules, config, opus, ytdl, youtubeSearch, me
             if (args[1].startsWith("http")) {
                 server.queue.push(args[1]);
                 message.reply("Adding " + args[1]);
-                youtube.youtube(bot, modules, config, opus, ytdl, youtubeSearch).play(server, message);
+                play(message);
             } else {
-                youtube.youtube(bot, modules, config, opus, ytdl, youtubeSearch, message).searchfunc(server, message);
+                searchfunc(message);
             }
         } else {
             message.react("❌");
             message.reply(`Please use \`${prefix}init\` before starting playing music`);
         }
     }
+
+    function play(message) {
+        connection.then(connexion => {
+            dispatcher = connexion.playStream(ytdl(server.queue[0], { filter: "audioonly" }));
+            server.queue.shift();
+            dispatcher.on("end", function () {
+                if (server.queue[0]) {
+                    play(server, message);
+                }
+                message.channel.send("Song Ended!");
+            });
+        });
+    };
+
+    function searchfunc(message) {
+        var opts = {
+            key: config.youtube_key,
+        }
+        var name = message.content.slice(6);
+        console.log(name);
+        youtubeSearch(name, opts, function (errors, results) {
+            if (errors) {
+                message.reaction = null;
+                message.react("❌");
+                message.reply("I got a problem adding `" + name + "` to the queue, please try again later");
+                return;
+            }
+            message.reply("Adding " + results[0].link);
+            server.queue.push(results[0].link)
+            message.react("✅");
+            play(server, message);
+        });
+    };
 }
