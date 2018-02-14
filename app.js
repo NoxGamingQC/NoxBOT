650b512d47e45dc4fd398e4014b6500e2e66ff75
Jimmy Béland-Bédard <jbeland@armrecouvrement.com>
2018-02-14 11:24:49 -0500

    Add a glyph code

--- app.js
+++ app.js
@@ -200,7 +200,7 @@ bot.on('message', function (message) {
                 description: 'You can redeem these Warframe promo codes on the in game market or on the website under the section promo codes',
                 fields: [{
                     name: "These are some Warframe Partner Glyphs code",
-                    value: "- IFLYNN\n- TACTICALPOTATO\n- MOGAMU\n- SKILLUP\n- ORIGINALWICKEDFUN\n- ADMIRALBAHROO\n- SP00NERISM\n- BIKEMAN\n- SUMMIT1G\n- KINGGOTTHALION\n- MCIK\n- TVSBOH\n- HOMIINVOCADO\n- BRICKY"
+                    value: "- IFLYNN\n- TACTICALPOTATO\n- MOGAMU\n- SKILLUP\n- ORIGINALWICKEDFUN\n- ADMIRALBAHROO\n- SP00NERISM\n- BIKEMAN\n- SUMMIT1G\n- KINGGOTTHALION\n- MCIK\n- TVSBOH\n- HOMIINVOCADO\n- BRICKY\n- N00BLSHOWTEK"
                 },
                 {
                     name: "Free Heat Sword and a weapon slot",
