6e2d76da538cfa26040cc519ae1f17f3ee4bc4c9
Jimmy Béland-Bédard <jbeland@armrecouvrement.com>
2018-02-22 14:14:06 -0500

    Fix commands

--- Modules/commands.js
+++ Modules/commands.js
@@ -1,8 +1,109 @@
 exports.commands = function (bot, config, message) {
     const prefix = config.prefix;
     if (message.content === prefix + 'commands') {
-        if(message.guild && message.guild.id == '282902357862514688') {
-            message.author.send({
+        if(message.guild) {
+            if(message.guild.id == '282902357862514688') { //nox
+                message.author.send({
+                    embed: {
+                        color: '4961603',
+                        author: {
+                            name: bot.user.username,
+                            icon_url: bot.user.avatarURL
+                        },
+                        title: bot.user.username + ' commands',
+                        description: message.author + ', there is the list of commands you can use!',
+                        fields: [{
+                            name: "​🌎 General commands",
+                            value: prefix+'commands: Get '+bot.user.username+' commands\n'+prefix+'avatar <mentionned user>: Show your avatar\n'+prefix+'invite: Create a 24h link to the server\n'+prefix+'lmgtfy <search terms>: Send you a LMGTFY link\n'+prefix+'ping: Show the '+bot.user.username+' ping'
+                        },
+                        {
+                            name: "🔗 Links commands",
+                            value: prefix+'psn: Get NoxRacing\'s PSN Username\n'+prefix+'steam: Get NoxRacing\'s Steam profile link\n'+prefix+'twitch: Get NoxRacing\'s Twitch channel link\n'+prefix+'xbl: Get NoxRacing\'s Xbox Live Username'
+                        },
+                        {
+                            name: "​🔐 Roles commands",
+                            value: prefix+'rank list: List of all joinable roles\n'+prefix+'rank join <role>: Make you join a role\n'+prefix+'rank leave <role>: Make you leave a role'
+                        },
+                        {
+                            name: "🎨 Colors commands",
+                            value: prefix+'color set <color>: Give you a color\n'+prefix+'color reset: Remove your color\n'+prefix+'color see <color>: Give you a demo of the color\n'+prefix+'color list: Give you a list of color'
+                        },
+                        {
+                            name: "⚔️ Moderations commands",
+                            value: prefix + 'timeout <user> <reason>: Mute the user for an undefined amount of time, the moderator must precise the reasons of the timeout'
+                        }],
+                        timestamp: new Date(),
+                        footer: {
+                            icon_url: bot.user.avatarURL,
+                            text: "© Copyright 2018 - NoxRacing"
+                        }
+                    }
+                });
+            } else if (message.guild.id == '343049869847429120') { //tiwill
+                message.author.send({
+                    embed: {
+                        color: '4961603',
+                        author: {
+                            name: bot.user.username,
+                            icon_url: bot.user.avatarURL
+                        },
+                        title: bot.user.username + ' commands',
+                        description: message.author + ', there is the list of commands you can use!',
+                        fields: [{
+                            name: "🌎 General commands",
+                            value: prefix + 'commands: Get ' + bot.user.username + ' commands'
+                        },
+                        {
+                            name: "​🔐 Roles commands",
+                            value: prefix + 'rank list: List of all joinable roles\n' + prefix + 'rank join <role>: Make you join a role\n' + prefix + 'rank leave <role>: Make you leave a role'
+                        },
+                        {
+                            name: "🎨 Colors commands",
+                            value: prefix + 'color set <color>: Give you a color\n' + prefix + 'color reset: Remove your color\n' + prefix + 'color see <color>: Give you a demo of the color\n' + prefix + 'color list: Give you a list of color'
+                        },
+                        {
+                            name: "⚔️ Moderations commands",
+                            value: prefix + 'timeout <user> <reason>: Mute the user for an undefined amount of time, the moderator must precise the reasons of the timeout'
+                        }],
+                        timestamp: new Date(),
+                        footer: {
+                            icon_url: bot.user.avatarURL,
+                            text: "© Copyright 2018 - NoxRacing"
+                        }
+                    }
+                });
+            } else {
+                message.author.send({
+                    embed: {
+                        color: '4961603',
+                        author: {
+                            name: bot.user.username,
+                            icon_url: bot.user.avatarURL
+                        },
+                        title: bot.user.username + ' commands',
+                        description: message.author + ', there is the list of commands you can use!',
+                        fields: [{
+                            name: "🌎 General commands",
+                            value: prefix + 'commands: Get ' + bot.user.username + ' commands'
+                        },
+                        {
+                            name: "​🔐 Roles commands",
+                            value: prefix + 'rank list: List of all joinable roles\n' + prefix + 'rank join <role>: Make you join a role\n' + prefix + 'rank leave <role>: Make you leave a role'
+                        },
+                        {
+                            name: "​🎨 Colors commands",
+                            value: prefix + 'color set <color>: Give you a color\n' + prefix + 'color reset: Remove your color\n' + prefix + 'color see <color>: Give you a demo of the color\n' + prefix + 'color list: Give you a list of color'
+                        }
+                        ],
+                        timestamp: new Date(),
+                        footer: {
+                            icon_url: bot.user.avatarURL,
+                            text: "© Copyright 2018 - NoxRacing"
+                        }
+                    }
+                });
+            }
+            message.channel.send({
                 embed: {
                     color: '4961603',
                     author: {
@@ -10,27 +111,7 @@ exports.commands = function (bot, config, message) {
                         icon_url: bot.user.avatarURL
                     },
                     title: bot.user.username + ' commands',
-                    description: message.author + ', there is the list of commands you can use!',
-                    fields: [{
-                        name: "​🌎 General commands",
-                        value: prefix+'commands: Get '+bot.user.username+' commands\n'+prefix+'avatar <mentionned user>: Show your avatar\n'+prefix+'invite: Create a 24h link to the server\n'+prefix+'lmgtfy <search terms>: Send you a LMGTFY link\n'+prefix+'ping: Show the '+bot.user.username+' ping'
-                    },
-                    {
-                        name: "🔗 Links commands",
-                        value: prefix+'psn: Get NoxRacing\'s PSN Username\n'+prefix+'steam: Get NoxRacing\'s Steam profile link\n'+prefix+'twitch: Get NoxRacing\'s Twitch channel link\n'+prefix+'xbl: Get NoxRacing\'s Xbox Live Username'
-                    },
-                    {
-                        name: "​🔐 Roles commands",
-                        value: prefix+'rank list: List of all joinable roles\n'+prefix+'rank join <role>: Make you join a role\n'+prefix+'rank leave <role>: Make you leave a role'
-                    },
-                    {
-                        name: "🎨 Colors commands",
-                        value: prefix+'color set <color>: Give you a color\n'+prefix+'color reset: Remove your color\n'+prefix+'color see <color>: Give you a demo of the color\n'+prefix+'color list: Give you a list of color'
-                    },
-                    {
-                        name: "⚔️ Moderations commands",
-                        value: prefix + 'timeout <user> <reason>: Mute the user for an undefined amount of time, the moderator must precise the reasons of the timeout'
-                    }],
+                    description: message.author + ', a list of commands have been sent to your private message, go check them out! :wink:',
                     timestamp: new Date(),
                     footer: {
                         icon_url: bot.user.avatarURL,
@@ -72,21 +153,5 @@ exports.commands = function (bot, config, message) {
         if (message.deletable) {
             message.delete();
         }
-        message.channel.send({
-            embed: {
-                color: '4961603',
-                author: {
-                    name: bot.user.username,
-                    icon_url: bot.user.avatarURL
-                },
-                title: bot.user.username + ' commands',
-                description: message.author + ', a list of commands have been sent to your private message, go check them out! :wink:',
-                timestamp: new Date(),
-                footer: {
-                    icon_url: bot.user.avatarURL,
-                    text: "© Copyright 2018 - NoxRacing"
-                }
-            }
-        });
     }
 }
--- app.js
+++ app.js
@@ -254,63 +254,71 @@ bot.on('message', function (message) {
 });
 
 bot.on('message', function (message) {
-    if (message.content === prefix + 'warframe codes') {
-        var codeList = '';
-        message.channel.send({
-            embed: {
-                color: '9846215',
-                author: {
-                    name: message.author.username,
-                    icon_url: message.author.avatarURL
-                },
-                title: 'Warframe promo codes',
-                description: 'You can redeem these Warframe promo codes on the in game market or on the website under the section promo codes',
-                fields: [{
-                    name: "These are some Warframe Partner Glyphs code",
-                    value: "- ADMIRALBAHROO\n- BIKEMAN\n- BRICKY\n- HOMIINVOCADO\n- IFLYNN\n- KINGGOTTHALION\n- MCIK\n- MOGAMU\n- N00BLSHOWTEK\n- ORIGINALWICKEDFUN\n- SKILLUP\n- SP00NERISM\n- SUMMIT1G\n- TACTICALPOTATO\n- TVSBOH"
-                },
-                {
-                    name: "Free Heat Sword and a weapon slot",
-                    value: "- FREESWORD"
-                },
-                {
-                    name: "Get a free Vectis, a free weapon slot and the Vectis Tekulu skin",
-                    value: "- FN6B-8RML-MLH6-GM2N"
-                }],
-                timestamp: new Date(),
-                footer: {
-                    icon_url: bot.user.avatarURL,
-                    text: "Code can be used only once by account"
+    if (message.guild) {
+        if (message.guild.id == '282902357862514688') {
+            if (message.content === prefix + 'warframe codes') {
+                var codeList = '';
+                message.channel.send({
+                    embed: {
+                        color: '9846215',
+                        author: {
+                            name: message.author.username,
+                            icon_url: message.author.avatarURL
+                        },
+                        title: 'Warframe promo codes',
+                        description: 'You can redeem these Warframe promo codes on the in game market or on the website under the section promo codes',
+                        fields: [{
+                            name: "These are some Warframe Partner Glyphs code",
+                            value: "- ADMIRALBAHROO\n- BIKEMAN\n- BRICKY\n- HOMIINVOCADO\n- IFLYNN\n- KINGGOTTHALION\n- MCIK\n- MOGAMU\n- N00BLSHOWTEK\n- ORIGINALWICKEDFUN\n- SKILLUP\n- SP00NERISM\n- SUMMIT1G\n- TACTICALPOTATO\n- TVSBOH"
+                        },
+                        {
+                            name: "Free Heat Sword and a weapon slot",
+                            value: "- FREESWORD"
+                        },
+                        {
+                            name: "Get a free Vectis, a free weapon slot and the Vectis Tekulu skin",
+                            value: "- FN6B-8RML-MLH6-GM2N"
+                        }],
+                        timestamp: new Date(),
+                        footer: {
+                            icon_url: bot.user.avatarURL,
+                            text: "Code can be used only once by account"
+                        }
+                    }
+                });
+                if (message.deletable) {
+                    message.delete();
                 }
             }
-        });
-        if (message.deletable) {
-            message.delete();
         }
     }
 });
 
 
 bot.on('message', function (message) {
-    if (message.content === prefix + 'kiwis') {
-        message.channel.send({
-            embed: {
-                color: '11982139',
-                author: {
-                    name: message.author.username,
-                    icon_url: message.author.avatarURL
-                },
-                title: 'Kiwis!!!',
-                description: '🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝',
-                timestamp: new Date(),
-                footer: {
-                    icon_url: bot.user.avatarURL,
-                    text: bot.user.username
+    if (message.guild) {
+        if (message.guild.id == '282902357862514688') {
+            if (message.content === prefix + 'kiwis') {
+                message.channel.send({
+                    embed: {
+                        color: '11982139',
+                        author: {
+                            name: message.author.username,
+                            icon_url: message.author.avatarURL
+                        },
+                        title: 'Kiwis!!!',
+                        description: '🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝',
+                        timestamp: new Date(),
+                        footer: {
+                            icon_url: bot.user.avatarURL,
+                            text: bot.user.username
+                        }
+                    }
+                });
+                if (message.deletable) {
+                    message.delete();
                 }
             }
-        });
-        if (message.deletable) {
-            message.delete();
         }
     }
 });
