57f65bdc666d8ceb232f959b0928b9a4c712a120
Jimmy Béland-Bédard <jbeland@armrecouvrement.com>
2018-02-22 11:28:20 -0500

    Change to embded lmgtfy

--- app.js
+++ app.js
@@ -230,8 +230,25 @@ bot.on('message', function (message) {
                     content += '+'+word;
                 }
             });
-            message.react("✅");
-            message.reply('Here is a link: http://lmgtfy.com/?q=' + content);
+            message.channel.send({
+                embed: {
+                    color: '11141120',
+                    author: {
+                        name: message.author.username,
+                        icon_url: message.author.avatarURL
+                    },
+                    title: 'LMGTFY Link',
+                    description: 'There is a link: http://lmgtfy.com/?q=' + content,
+                    timestamp: new Date(),
+                    footer: {
+                        icon_url: bot.user.avatarURL,
+                        text: bot.user.username
+                    }
+                }
+            });
+            if (message.deletable) {
+                message.delete();
+            }
         }
     }
 });
