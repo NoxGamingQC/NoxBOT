a1ca2efb13acb779d6821f0fd1697687ca1deef1
Jimmy Béland-Bédard <jbeland@armrecouvrement.com>
2018-02-22 11:38:52 -0500

    Fix kiwi command

--- app.js
+++ app.js
@@ -293,7 +293,25 @@ bot.on('message', function (message) {
 
 bot.on('message', function (message) {
     if (message.content === prefix + 'kiwis') {
-        message.channel.send('🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝');
+        message.channel.send({
+            embed: {
+                color: '11982139',
+                author: {
+                    name: message.author.username,
+                    icon_url: message.author.avatarURL
+                },
+                title: 'Kiwis!!!',
+                description: '🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝🥝',
+                timestamp: new Date(),
+                footer: {
+                    icon_url: bot.user.avatarURL,
+                    text: bot.user.username
+                }
+            }
+        });
+        if (message.deletable) {
+            message.delete();
+        }
     }
 });
 
