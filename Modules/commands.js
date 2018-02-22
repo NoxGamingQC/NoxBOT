c7a137b252a048308a2887128d2d675bb31be5c6
Jimmy Béland-Bédard <jbeland@armrecouvrement.com>
2018-02-22 13:17:52 -0500

    Fix commands text format

--- Modules/commands.js
+++ Modules/commands.js
@@ -72,6 +72,21 @@ exports.commands = function (bot, config, message) {
         if (message.deletable) {
             message.delete();
         }
-        message.reply(`List of commands have been sent to your private message, go check them out! :wink:`);
+        message.channel.send({
+            embed: {
+                color: '4961603',
+                author: {
+                    name: bot.user.username,
+                    icon_url: bot.user.avatarURL
+                },
+                title: bot.user.username + ' commands',
+                description: message.author + ', a list of commands have been sent to your private message, go check them out! :wink:',
+                timestamp: new Date(),
+                footer: {
+                    icon_url: bot.user.avatarURL,
+                    text: "© Copyright 2018 - NoxRacing"
+                }
+            }
+        });
     }
 }
