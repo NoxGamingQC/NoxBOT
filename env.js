7393ab92e1fe0e686187144ba166d90c2f2f9196
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-12 14:50:40 -0400

    Updating noxbot

---
+++ env.js
@@ -0,0 +1,20 @@
+exports.discord = {
+    author_name: "NoxGamingQC 🇨🇦#5798",
+    website: "noxgamingqc.ca",
+    environement: "development",
+    client_id: "",
+    bot_token: "MTAyOTgxMzk2OTkwNjU2OTI1Nw.GFzu2F.avPQO4L0S2v5IPmhE04UiKEmAf0OChLGznwTrQ",
+    prod_bot_token: "OTQxODg4OTkzODg3NDE2MzQw.GGoagU.9QE37tfaMWNiNAwFGiG_0w-91oaLtAzC97LI1w",
+    twitch_token: "",
+    youtube_key: "",
+    db_url: "",
+    prefix: "n!",
+    discord_invite: "https://noxgamingqc.ca/discord",
+    discordStatus: "dnd",
+    embedColor: {
+        success: "4437377",
+        warning: "16766720",
+        error: "16711680",
+        twitch: "6570404"
+    }
+};
