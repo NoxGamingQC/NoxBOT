614702b79ad5202c5d8cc7de6192ee71b93697c0
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-13 10:08:05 -0400

    Fix NoxBot

--- noxbot.js
+++ noxbot.js
@@ -25,54 +25,21 @@ global.lastError = null;
 console.log('Creating Discord Client');
 global.bot = new Discord.Client();
 
-/*process.on('uncaughtException', (error) => {
-    reportError.reportError(error, 500, 'You have triggered an uncaughtException:')
-})
-
-process.on('unhandledRejection', (error) => {
-    if(error.code === "ECONNRESET") {
-        reportError.reportError(error, 500, 'You have triggered an ECONNRESET error. NoxBOT will now reconnect.')
-    
-        bot.connect();
-    } else if(error.code === "EAI_AGAIN") {
-        reportError.reportError(error, 500, 'You have triggered an EAI_AGAIN error. NoxBOT will now reconnect.')
-    
-        bot.connect();
-    } else {
-        reportError.reportError(error, 500, 'You have triggered an unhandledRejection, you may have forgotten to catch a Promise rejection:')
-    }
-})*/
-
-/*function isEmoji(str) {
-    var ranges = [
-        '(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])'
-    ];
-    if(str) {
-        if (str.match(ranges.join('|'))) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false
-    }
-}*/
 
 bot.login(discordToken);
 
-bot.on('debug', console.log)
+bot.on('debug', console.log);
 
 bot.on('ready', function () {
     console.log("Bot Launched...");
-    //var username = bot.user.username;
+    bot.user.setStatus(env.discord.bot_status);
+    bot.user.setActivity('Back online.');
     //bot.user.setAvatar('img/avatar/' + username + '\'s avatar.png');
 
     /*updateByTime();
     reactionRoles();
 
     global.isDev = config.development;
-    bot.user.setStatus((isDev ? 'dnd' : 'Online'));
-    bot.user.setActivity(bot.user.username + ' is back online.');
     /*if (!isDev) {
         TwitchClient.on('connected', function () {
             console.log('Connected to Twitch');
