f8541d5297c6fa4cf4e9237262d2c7874fa70562
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-17 14:33:30 -0400

    Fix ajax

--- Modules/pointSystem.js
+++ Modules/pointSystem.js
@@ -3,12 +3,12 @@ exports.addPoints = function(userID, comment) {
         url: process.env.WEBSITE_BASE_LINK + "points/add",
         type: "post",
         data: {
-            'token': process.env.WEBSITE_TOKEN,
-            'botID': bot.user.id,
-            'userID': userID,
-            'points': process.env.BASE_POINT,
-            'mulitplier': process.env.POINT_MULTIPLIER,
-            'comment': comment
+            token: process.env.WEBSITE_TOKEN,
+            botID: bot.user.id,
+            userID: userID,
+            points: process.env.BASE_POINT,
+            mulitplier: process.env.POINT_MULTIPLIER,
+            comment: comment
         },
         error: function(error){
             console.log(error);
