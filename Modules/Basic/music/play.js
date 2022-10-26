a2ea85fb1590222590a5773a56c014954b278949
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-26 15:42:58 -0400

    Add music player

--- Modules/Basic/music/play.js
+++ Modules/Basic/music/play.js
@@ -1,12 +1,12 @@
 exports.command = function(message, ytdl, streamOptions) {
-    if (message.content === process.env.PREFIX + 'play') {
-        var search = message.split(process.env.PREFIX + 'play ')[1];
+    if (message.content.includes(process.env.PREFIX + 'play')) {
+        var search = message.content.split(process.env.PREFIX + 'play ')[1];
         var url = '';
         if(ytdl.validateURL(search)) {
             url = search;
         }
         if(global.connection) {
-            if(url) {
+            if(url.length > 0) {
                 stream = ytdl(url, { filter : 'audioonly' });
                 global.connection.playStream(stream, streamOptions);
             } else {
