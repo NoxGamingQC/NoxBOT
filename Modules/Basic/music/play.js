a402edcb8a22424d0be206c1b7ae742047c660a5
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-26 15:30:26 -0400

    Try to fix it

--- Modules/Basic/music/play.js
+++ Modules/Basic/music/play.js
@@ -1,8 +1,17 @@
 exports.command = function(message, ytdl, streamOptions) {
     if (message.content === process.env.PREFIX + 'play') {
+        var search = message.split(process.env.PREFIX + 'play ')[1];
+        var url = '';
+        if(ytdl.validateURL(search)) {
+            url = search;
+        }
         if(global.connection) {
-            var stream = ytdl('https://www.youtube.com/watch?v=l482T0yNkeo', { filter : 'audioonly' });
-            global.connection.playStream(stream, streamOptions);
+            if(url) {
+                stream = ytdl(url, { filter : 'audioonly' });
+                global.connection.playStream(stream, streamOptions);
+            } else {
+                message.reply('Song not found.');
+            }
         } else {
             message.reply('Please join a voice channel and run the command: `'+ process.env.PREFIX +'init`');
         }
