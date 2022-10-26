e2265b24afcb252a5b9dc237ccca760817817235
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-26 15:13:14 -0400

    Enable play

--- Modules/Basic/music/index.js
+++ Modules/Basic/music/index.js
@@ -1,9 +1,11 @@
 var init = require("./init.js");
 var leave = require("./leave.js");
 var play = require("./play.js");
+var ytdl = require('ytdl-core');
+var streamOptions = { seek: 0, volume: 1 };
 
 exports.module = function(message) {
     init.command(message);
-    play.command(message);
+    play.command(message, ytdl, streamOptions);
     leave.command(message);
 };
--- Modules/Basic/music/play.js
+++ Modules/Basic/music/play.js
@@ -1,7 +1,8 @@
-exports.command = function(message) {
+exports.command = function(message, ytdl, streamOptions) {
     if (message.content === process.env.PREFIX + 'play') {
         if(global.connection) {
-            
+            var stream = ytdl('https://www.youtube.com/watch?v=l482T0yNkeo', { filter : 'audioonly' });
+            global.connection.playStream(stream, streamOptions);
         } else {
             message.reply('Please join a voice channel and run the command: `'+ process.env.PREFIX +'init`');
         }
