40af0b38747476e57e7dd6b163a5313f5a00b58c
NoxGamingQC <noxgamingqc@gmail.com>
2022-10-26 09:54:42 -0400

    Ping

---
+++ Modules/Basic/utilities/ping.js
@@ -0,0 +1,29 @@
+exports.command = function(message) {
+    if (message.content === process.env.PREFIX + 'ping') {
+        var server = message.guild;
+        if (server) {
+            message.channel.send({
+                embed: {
+                    color: process.env.SUCCESS_COLOR,
+                    author: {
+                        name: bot.user.username + ' - Ping',
+                        icon_url: bot.user.avatarURL
+                    },
+                    thumbnail: {
+                        url: bot.user.avatarURL
+                    },
+                    title: 'Pong',
+                    description: `🏓 Latency is ${message.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`,
+                    timestamp: new Date(),
+                    footer: {
+                        icon_url: message.author.avatarURL,
+                        text: message.author.tag
+                    }
+                }
+            });
+        }
+        if (message && message.deletable) {
+            message.delete();
+        }
+    }
+}
--- Modules/index.js
+++ Modules/index.js
@@ -1,13 +1,11 @@
 var inviteModule = require("./Basic/utilities/invite.js");
+var pingModule = require("./Basic/utilities/ping.js");
 var helpModule = require("./Basic/help/help.js");
 var pokemon = require("./Basic/pokemon/index.js");
 
 exports.commands = function(message) {
     inviteModule.command(message);
+    pingModule.command(message);
     helpModule.command(message);
     pokemon.module(message);
-
-    if(message == process.env.PREFIX + "ping") {
-        message.channel.send("pong");
-    }
 };
