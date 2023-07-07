f8e40c7df713de66aa4bcea6e5481bcff12edf06
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-07 15:31:53 -0400

    Limit number of instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [{
       name: "app",
       script: "./noxbot.js",
-      instances: "max",
+      instances: "1",
       env: {
         NODE_ENV: "development",
       },
