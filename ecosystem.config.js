2f49edb6125cb1cec07c7e0e14e2983168147a69
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-07 15:23:37 -0400

    Fix script name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
       name: "app",
-      script: "./app.js",
+      script: "./noxbot.js",
       instances: "max",
       env: {
         NODE_ENV: "development",
