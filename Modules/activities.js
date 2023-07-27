acab46cac2fb503436ab43b006338ac86c91c017
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-27 12:59:38 -0400

    Adding console log

--- Modules/activities.js
+++ Modules/activities.js
@@ -4,10 +4,12 @@ exports.setActivity = function(bot) {
     var url = process.env.WEBSITE_BASE_LINK + "activities";
     var activityResponse = request.get(url, "GET");
     console.log(activityResponse)
-    if(activityResponse['code'] == 200) {
+    if(activityResponse.code == 200) {
         const keys = Object.keys(activityResponse['response']);
         const randIndex = Math.floor(Math.random() * keys.length);
         const randKey = keys[randIndex];
         bot.user.setActivity(activityResponse['response'][randKey]);
+    } else {
+        console.log('Can\'t get activities from: ' + url);
     }
 };
