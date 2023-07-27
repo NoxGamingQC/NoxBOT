6d2d3b2e1bcb4b25d39c12d0b3c1dd815544cdd6
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-27 12:43:44 -0400

    Take 2

--- Modules/activities.js
+++ Modules/activities.js
@@ -3,10 +3,10 @@ var request = require("../request.js");
 exports.setActivity = function(bot) {
     var url = process.env.WEBSITE_BASE_LINK + "activities";
     var activityResponse = request.get(url, "GET");
-    if(activityResponse.code == 200) {
-        const keys = Object.keys(activityResponse.response);
+    if(activityResponse['code'] == 200) {
+        const keys = Object.keys(activityResponse['response']);
         const randIndex = Math.floor(Math.random() * keys.length);
         const randKey = keys[randIndex];
-        bot.user.setActivity(activityResponse.response[randKey]);
+        bot.user.setActivity(activityResponse['response'][randKey]);
     }
 };
