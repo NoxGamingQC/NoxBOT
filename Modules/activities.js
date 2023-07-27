3307aee6b9788d9a53fcfdd13d8f0bc87f533b7f
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-27 11:58:36 -0400

    Adding request.js and testing it with activities

--- Modules/activities.js
+++ Modules/activities.js
@@ -1,15 +1,12 @@
+var request = require("../request.js");
+
 exports.setActivity = function(bot) {
-    $.ajax({
-        url: process.env.WEBSITE_BASE_LINK + "activities",
-        type: "GET",
-        success: function(result){
-            const keys = Object.keys(result);
-            const randIndex = Math.floor(Math.random() * keys.length);
-            const randKey = keys[randIndex];
-            bot.user.setActivity(result[randKey]);
-        },
-        error: function(error){
-            console.log(error);
-        }
-    });
+    var url = process.env.WEBSITE_BASE_LINK + "activities";
+    var activityResponse = request.get(url, "GET");
+    if(activityResponse.code == 200) {
+        const keys = Object.keys(activityResponse.response);
+        const randIndex = Math.floor(Math.random() * keys.length);
+        const randKey = keys[randIndex];
+        bot.user.setActivity(activityResponse.response[randKey]);
+    }
 };
---
+++ request.js
@@ -0,0 +1,22 @@
+
+exports.get = function(url, requestType, message = null) {
+    $.ajax({
+        url: url,
+        type: requestType,
+        success: function (response) {
+            return {
+                code: 200,
+                response: response
+            }
+        },
+        error: function(error){
+            if(message) {
+                message.channel.send('An error occured while trying to search for the pokemon species. Please contact us @ noxgamingqc.ca and submit a bug report.');
+            }
+            return {
+                code: 500,
+                response: error
+            }
+        }
+    });
+}
