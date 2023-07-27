5fa3339f13bd281e5ba7d17edfe9075e96a67087
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-27 12:47:26 -0400

    console log response

--- Modules/activities.js
+++ Modules/activities.js
@@ -3,6 +3,8 @@ var request = require("../request.js");
 exports.setActivity = function(bot) {
     var url = process.env.WEBSITE_BASE_LINK + "activities";
     var activityResponse = request.get(url, "GET");
+    console.log(activityResponse['code'])
+    console.log(activityResponse.code)
     if(activityResponse['code'] == 200) {
         const keys = Object.keys(activityResponse['response']);
         const randIndex = Math.floor(Math.random() * keys.length);
