ff64e1e1df1c19ba0f6ae9d600d9fee80f45060a
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-27 14:11:37 -0400

    Try to handle data differently

--- Modules/activities.js
+++ Modules/activities.js
@@ -2,10 +2,12 @@ var request = require("../request.js");
 
 exports.setActivity = function(bot) {
     var url = process.env.WEBSITE_BASE_LINK + "activities";
-    activityHandler(request.get(url, "GET"));
+    result = request.get(url, "GET")
+    activityHandler(result);
 };
 
 function activityHandler(result) {
+    _callback();    
     const keys = Object.keys(result);
     const randIndex = Math.floor(Math.random() * keys.length);
     const randKey = keys[randIndex];
