e2bbea3806f435a09563639e7d5d87c09928ec14
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-27 13:42:06 -0400

    Trying to fix result

--- Modules/activities.js
+++ Modules/activities.js
@@ -2,15 +2,12 @@ var request = require("../request.js");
 
 exports.setActivity = function(bot) {
     var url = process.env.WEBSITE_BASE_LINK + "activities";
-    var activityResponse = request.get(url, "GET");
-    console.log('URL: ' + url);
-    console.log('Response: ' + activityResponse)
-    if(activityResponse.code == 200) {
-        const keys = Object.keys(activityResponse['response']);
+    request.get(url, "GET").then((result) => {
+        const keys = Object.keys(result.response);
         const randIndex = Math.floor(Math.random() * keys.length);
         const randKey = keys[randIndex];
-        bot.user.setActivity(activityResponse['response'][randKey]);
-    } else {
-        console.log('Can\'t get activities from: ' + url);
-    }
+        bot.user.setActivity(result.response[randKey]);
+    }).catch((error) => {
+        console.log(error);
+    })
 };
