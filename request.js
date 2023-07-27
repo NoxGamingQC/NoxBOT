39ae75576d37a4817f2fff1cf5c040c0658597a8
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-27 12:52:00 -0400

    console log response

--- request.js
+++ request.js
@@ -5,8 +5,8 @@ exports.get = function(url, requestType, message = null) {
         type: requestType,
         success: function (response) {
             return {
-                code: 200,
-                response: response
+                'code': 200,
+                'response': response
             }
         },
         error: function(error){
@@ -14,8 +14,8 @@ exports.get = function(url, requestType, message = null) {
                 message.channel.send('An error occured while trying to search for the pokemon species. Please contact us @ noxgamingqc.ca and submit a bug report.');
             }
             return {
-                code: 500,
-                response: error
+                'code': 500,
+                'response': error
             }
         }
     });
