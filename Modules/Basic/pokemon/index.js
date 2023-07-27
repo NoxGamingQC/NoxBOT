5a8d404f61582a86292ec78629870ec505581dd8
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-27 10:15:09 -0400

    Remove unecessary file for the pokemon module

--- Modules/Basic/pokemon/index.js
+++ Modules/Basic/pokemon/index.js
@@ -1,8 +1,11 @@
-var pokemon = require("./pokemon.js");
-var shiny = require("./shiny.js");
-
+var getPokemon = require("./getPokemon.js");
 
 exports.module = function(message) {
-    pokemon.command(message);
-    shiny.command(message);
+    if (message.content.includes(process.env.PREFIX + 'pokemon')) {
+        var search = message.content.split(process.env.PREFIX + 'pokemon ', 2)[1];
+        getPokemon.response(isShiny = false, search);
+    } else if(message.content.includes(process.env.PREFIX + 'shiny')) {
+        var search = message.content.split(process.env.PREFIX + 'pokemon ', 2)[1];
+        getPokemon.response(isShiny = true, search);
+    }
 };
