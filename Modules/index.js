fe85d36b2074fad4860dfca9f0c141ca8a2ded9f
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-28 13:10:53 -0400

    Fix error with minecraft module not loading!

--- Modules/index.js
+++ Modules/index.js
@@ -3,6 +3,7 @@ var help = require("./Basic/help/help.js");
 var music = require("./Basic/music/index.js");
 var pokemon = require("./Basic/pokemon/index.js");
 var utility = require("./Basic/utilities/index.js");
+var minecraft = require("./Basic/minecraft/index.js");
 
 exports.modules = function(message) {
     help.command(message);
