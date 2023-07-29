dbf555526419cffe500db440884c6c046259a16f
NoxGamingQC <noxgamingqc@gmail.com>
2023-07-29 13:24:23 -0400

    Pass prefix in functino

--- noxbot.js
+++ noxbot.js
@@ -52,6 +52,7 @@ function handleMessage(message, result) {
             pointSystem.addPoints(message.author.id, comment);
         }
     }
+    console.log(result.prefix);
     modules.modules(message, result.prefix);
 }
 
