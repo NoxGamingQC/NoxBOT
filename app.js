d75e81fa468adc41c9378b6b1f75baf879be7b9e
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2024-12-21 08:01:59 -0500

    Reset NoxBOT to use ES6. Modules is currently under maintenance

--- app.js
+++ app.js
@@ -1,57 +1,22 @@
-var Discord = require('discord.js');
-var modules = require("./Modules/index.js");
-require('dotenv').config();
-var activities = require('./Modules/activities.js');
-var pointSystem = require('./Modules/pointSystem.js');
-var discordUserList = require('./Modules/discordUserList.js');
-var discordServerList = require('./Modules/discordServerList.js');
-var { JSDOM } = require( "jsdom" );
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv'
+import { JSDOM } from 'jsdom';
 var { window } = new JSDOM( "" );
-var jQuery = require('jquery')(window);
-var { document } = (new JSDOM('')).window;
 
-global.document = document;
-global.$ = jQuery;
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+dotenv.config({ path: './.env' });
 
-console.log('Creating Discord Client');
-global.discordBot = new Discord.Client();
-discordBot.login(process.env.BOT_TOKEN);
-
-discordBot.on('ready', function () {
-    console.log('Connected to Discord as ' + discordBot.user.username);
-    discordBot.user.setStatus(process.env.BOT_STATUS);
-    activities.setActivity(discordBot);
-    updateByTime();
-    discordUserList.loadChangeEvent(discordBot);
+client.on(Events.ClientReady, readyClient => {
+    console.log(`Logged in as ${readyClient.user.tag}!`);
+    client.user.setStatus(process.env.BOT_STATUS);
 });
 
-discordBot.on('disconnect', function(errMsg, code) {
-    console.log('▬▬▬▬▬ Bot Disconnected from Discord with code', code, 'for reason:', errMsg);
-    discordBot.connect();
-});
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
 
-discordBot.on('message', function (message) {
-    discordUserList.updateUser(message.author.id, message.author.username, message.author.avatarURL)
-    if(message.guild) {
-        discordServerList.getServerConfig(message.guild, message, handleMessage);
-        discordServerList.updateServer(message.guild.id, message.guild.name, message.guild.iconURL)
-    } else {
-        modules.loadAllModules(message, process.env.PREFIX);
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
     }
 });
 
-function handleMessage(message, result) {
-    if(pointSystem.isActive()) {
-        if(result.can_receive_points) {
-            var comment = "Chatting in a Discord server: " + message.guild.name + ' (' + message.guild.id + ')';
-            pointSystem.addPoints(message.author.id, comment);
-        }
-    }
-    modules.loadAllModules(message, result.prefix);
-}
-
-function updateByTime() {
-    setInterval(function () {
-        activities.setActivity(discordBot);
-    }, process.env.UPDATE_TIME);
-}
+client.login(process.env.BOT_TOKEN);
