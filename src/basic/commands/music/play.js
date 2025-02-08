6d50ab61672211939fb2508fbe5421cf3a91bb0c
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2025-02-08 08:50:47 -0500

    Add old music module in comment, will be deleted later

--- src/basic/commands/music/play.js
+++ src/basic/commands/music/play.js
@@ -1,6 +1,6 @@
 import { Events, MessageFlags } from 'discord.js';
 import { createAudioPlayer } from '@discordjs/voice';
-
+import youtubeSearchAPI from 'youtube-search-api';
 import  fs from 'fs';
 import ytdl from 'ytdl-core';
 
@@ -33,3 +33,52 @@ export default function play (client, commands) {
         }
     });
 }
+
+
+
+/*
+const { createreadstream } = require('fs');
+const { join } = require('path');
+const { createaudioresource, streamtype, createaudioplayer, joinvoicechannel } = require('@discordjs/voice');
+
+exports.command = function(message, streamOptions) {
+    if (message.content.includes(process.env.PREFIX + 'play')) {
+        var search = message.content.split(process.env.PREFIX + 'play ')[1];
+        var url = '';
+        if(!global.connection) {
+            init.init(message);
+        }
+        
+        const player = createaudioplayer()
+        joinvoicechannel({
+            channelid: message.member.voice.channel.id,
+            guildid: message.guild.id,
+            adaptercreator: message.guild.voiceadaptercreator
+        }).subscribe(player)
+        if(ytdl.validateURL(search)) {
+            url = search;
+            if(ytdl.validateURL(url) && search.length > 0) {
+                stream = ytdl(url,streamOptions).on('info', (info) => {
+                    message.channel.send('🎵 Now playing: `' + info.videoDetails.title + '`');
+                });
+                
+                player.play(stream)
+            } else {
+                message.reply('Song not found.');
+            }
+        } else {
+            youtubeSearchAPI.GetListByKeyword(search, false, 1, [{type:'video'}])
+            .then((response) => {
+                message.channel.send('🎵 Now playing: `' + response.items[0].title + '`');
+                url = 'https://www.youtube.com/watch?v=' + response.items[0].id;
+                if(ytdl.validateURL(url) && search.length > 0) {
+                    stream = ytdl(url,streamOptions);
+                    player.play(stream)
+                } else {
+                    message.reply('Song not found.');
+                }
+            });
+        }
+    }
+};
+*/
