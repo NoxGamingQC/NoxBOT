3be1a8308c6c62d7432f4d253add25ee0b98c0fd
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2025-02-07 23:57:20 -0500

    Try to play

--- src/basic/commands/music/leave.js
+++ src/basic/commands/music/leave.js
@@ -12,12 +12,12 @@ export default function play (client, commands) {
             try {
                 if (global.connection) { 
                     global.connection.destroy();
-                    interaction.editReply({ content: `Channel left.`, ephemeral: true});
+                    interaction.editReply({ content: `:wave: Channel left.`, ephemeral: true});
                 } else {
-                    interaction.editReply({ content: `Am I in a voice chat? I can't find the connection. To fix this issue, connect to a voice channel, then use command \`/init\`, so I can reset the connection then do the command \`/leave.\`. If the error persist, please contact a developer. Thank you.`, ephemeral: true});
+                    interaction.editReply({ content: `:thinking: Am I in a voice chat? I can't find the connection. To fix this issue, connect to a voice channel, then use command \`/init\`, so I can reset the connection then do the command \`/leave.\`. If the error persist, please contact a developer. Thank you.`, ephemeral: true});
                 }    
             } catch {
-                interaction.editReply({ content: `Am I in a voice chat? I can't find the connection. To fix this issue, connect to a voice channel, then use command \`/init\`, so I can reset the connection then do the command \`/leave.\`. If the error persist, please contact a developer. Thank you.`, ephemeral: true});
+                interaction.editReply({ content: `:thinking: Am I in a voice chat? I can't find the connection. To fix this issue, connect to a voice channel, then use command \`/init\`, so I can reset the connection then do the command \`/leave.\`. If the error persist, please contact a developer. Thank you.`, ephemeral: true});
             }
         }
     });
--- src/basic/commands/music/play.js
+++ src/basic/commands/music/play.js
@@ -1,4 +1,10 @@
 import { Events } from 'discord.js';
+import { createAudioPlayer } from '@discordjs/voice';
+
+import  fs from 'fs';
+import ytdl from 'ytdl-core';
+
+
 
 export default function play (client, commands) {
     commands.push({
@@ -8,9 +14,18 @@ export default function play (client, commands) {
 
     client.on(Events.InteractionCreate, async interaction  => {
         if (interaction.commandName === 'play') {
-            await interaction.deferReply({ephemeral: true});
-            
-            interaction.editReply({ content: `Song: ? , is currently playing`, ephemeral: true});
+            await interaction.deferReply({epohemeral: true});
+            try {
+                var audioPlayer = createAudioPlayer()
+                global.connection.subscribe(audioPlayer);
+                console.log(interaction);
+                var args = interaction.content.split(' ').slice(1)
+                audioPlayer.play(ytdl(args.join(" ")))
+                interaction.editReply({ content: `Song: ` + args.join(" ") + ` , is currently playing`, ephemeral: true});
+            } catch (error) {
+                console.log(error);
+                interaction.editReply({ content: `An error occured when trying to play a song.`, ephemeral: true});
+            }
         }
     });
 }
