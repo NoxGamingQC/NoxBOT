6fd5ff078b39ba2f4ef2494ead5e36181b9efa1e
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2025-02-07 23:28:00 -0500

    Add command to leave the channel

--- src/basic/commands/music/init.js
+++ src/basic/commands/music/init.js
@@ -10,20 +10,19 @@ export default function play (client, commands) {
     client.on(Events.InteractionCreate, async interaction  => {
         if (interaction.commandName === 'init') {
             await interaction.deferReply({ephemeral: true});
-            console.log(interaction.member.voice.channel);
             try {
                 if(interaction.member.voice.channel) {
-                    const connection = joinVoiceChannel({
+                    global.connection = joinVoiceChannel({
                         channelId: interaction.member.voice.channel.id,
                         guildId: interaction.member.voice.guild.id,
                         adapterCreator: interaction.member.voice.guild.voiceAdapterCreator
                     });
-                    currentInteraction.editReply({ content: `:smile: Connected to voice chat`, ephemeral: true});
+                    interaction.editReply({ content: `:smile: Connected to voice chat`, ephemeral: true});
                 } else {
-                    currentInteraction.editReply({ content: `:face_with_spiral_eyes: I had some trouble finding your voice channel. Are you in one?`, ephemeral: true});
+                    interaction.editReply({ content: `:face_with_spiral_eyes: I had some trouble finding your voice channel. Are you in one?`, ephemeral: true});
                 }
             } catch (error) {
-                currentInteraction.editReply({ content: `:face_with_spiral_eyes: I'm dizzy. Can you send this error to a developper. Thank you. /init - `+ error, ephemeral: true});
+                interaction.editReply({ content: `:face_with_spiral_eyes: I'm dizzy. Can you send this error to a developper. Thank you. Error happen durring \`/init\``, ephemeral: true});
             }
             
         }
--- src/basic/commands/music/leave.js
+++ src/basic/commands/music/leave.js
@@ -9,10 +9,14 @@ export default function play (client, commands) {
     client.on(Events.InteractionCreate, async interaction  => {
         if (interaction.commandName === 'leave') {
             await interaction.deferReply({ephemeral: true});
-            if (global.connection) { 
-                global.connection.destroy();
-                interaction.editReply({ content: `Channel left.`, ephemeral: true});
-            } else {
+            try {
+                if (global.connection) { 
+                    global.connection.destroy();
+                    interaction.editReply({ content: `Channel left.`, ephemeral: true});
+                } else {
+                    interaction.editReply({ content: `Am I in a voice chat? I can't find the connection. To fix this issue, connect to a voice channel, then use command \`/init\`, so I can reset the connection then do the command \`/leave.\`. If the error persist, please contact a developer. Thank you.`, ephemeral: true});
+                }    
+            } catch {
                 interaction.editReply({ content: `Am I in a voice chat? I can't find the connection. To fix this issue, connect to a voice channel, then use command \`/init\`, so I can reset the connection then do the command \`/leave.\`. If the error persist, please contact a developer. Thank you.`, ephemeral: true});
             }
         }
