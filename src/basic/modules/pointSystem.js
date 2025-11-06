9fdebd0b85d42693e681d478e68f1c0540373a82
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-11-06 07:09:17 -0500

    Add route to add points

--- src/basic/modules/pointSystem.js
+++ src/basic/modules/pointSystem.js
@@ -3,7 +3,7 @@ import { Events, MessageFlags, EmbedBuilder } from 'discord.js';
 export default function pointSystem (client, commands) {
    client.on('messageCreate', async message => {
         let randomNumber= Math.floor(Math.random() * 10);
-        if(randomNumber > -1) { //30% chance to get points
+        if(randomNumber > 7) { //30% chance to get points
             console.log("Sending points data...");
             let response = await fetch(process.env.WEBSITE_API_LINK + '/points/add', {
             method: 'POST',
