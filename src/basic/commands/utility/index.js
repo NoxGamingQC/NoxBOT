2eb1a2fd0500f86bb31b56dbea292e1d17317612
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-11-06 12:57:09 -0500

    Fix /user command

--- src/basic/commands/utility/index.js
+++ src/basic/commands/utility/index.js
@@ -1,7 +1,9 @@
 import invite from './invite.js';
 import ping from './ping.js';
+import user from './user.js';
 
 export default function utility(client, commands) {
     invite(client, commands);
     ping(client, commands);
+    user(client, commands);
 }
--- src/basic/commands/utility/user.js
+++ src/basic/commands/utility/user.js
@@ -1,46 +1,33 @@
-import { Events, MessageFlags } from 'discord.js';
+import { Events, MessageFlags, EmbedBuilder } from 'discord.js';
 
-export default function ping (client, commands) {
+export default function user (client, commands) {
     commands.push({
         name: 'user',
-        description: 'Display information about you!',
+        description: 'Display information about you!'
     });
-    var statusType = {
-      '0': 'Playing ',
-      '1': 'Streaming ',
-      '2': 'Listening ',
-      '3': 'Watching ',
-    };
 
-    var statusColor = {
-        'online': 4437377,
-        'idle': 16426522,
-        'dnd': 15746887,
-        'offline': 7634829,
-    
-    };
     client.on(Events.InteractionCreate, async interaction  => {
         if (interaction.commandName === 'user') {
             await interaction.deferReply({ephemeral: true});
-
-            const reply = await interaction.fetchReply();
+            const member = interaction.guild.members.cache.get(interaction.user.id);
+            const presence = member.presence;
             const userInformation = new EmbedBuilder()
                 //.setColor(data.data.color)
-                .setTitle(interaction.user.name)
-                .setAuthor({ name: client.user.username + ' - USER', iconURL: client.user.displayAvatarURL({ dynamic: true, size: 1024 }) })
-                .setDescription('Comming soon')
+                .setTitle(interaction.user.globalName)
+                .setAuthor({ name: interaction.user.globalName + ' - USER', iconURL: interaction.user.displayAvatarURL({ dynamic: true, size: 1024 }) })
+                .setDescription(presence ? presence.activities.map(activity => activity.name).join(', ') : 'No current activities')
                 .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true, size: 1024 }))
                 .addFields(
-                    { name: 'id', value: '' },
-                    { name: 'Creation date', value: '', inline: true },
-                    { name: 'Bot', value: '', inline: true },
-                    { name: 'Streaming', value: '', inline: true },
-                    { name: 'Status', value: '', inline: true },
-                    { name: 'Device', value: '', inline: true }
+                    { name: 'id', value: interaction.user.id, inline: true },
+                    { name: 'Bot', value: interaction.user.bot ? 'Yes' : 'No', inline: true },
+                    { name: 'Username', value: '@' + interaction.user.username, inline: true },
+                    { name: 'Status', value: presence ? presence.status : 'offline', inline: true },
+                    { name: 'Desktop', value: presence ? (presence.clientStatus.desktop ? presence.clientStatus.desktop : 'Offline') : 'Offline', inline: true },
+                    { name: 'Mobile', value: presence ? (presence.clientStatus.mobile ? presence.clientStatus.mobile : 'Offline') : 'Offline', inline: true }
                 )
                 .setTimestamp()
                 .setFooter({ text: interaction.user.globalName, iconURL: interaction.user.displayAvatarURL({ dynamic: true, size: 1024 }) });
-            interaction.editReply({ embeds: [userInformation], flags: MessageFlags.Ephemeral});
+                interaction.editReply({ embeds: [userInformation], flags: MessageFlags.Ephemeral});
         }
     });
 }
