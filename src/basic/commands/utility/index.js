1e4c6f2c084efd2b0440192c23838c6905c7df6d
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-11-06 21:24:38 -0500

    Add server command

--- src/basic/commands/utility/index.js
+++ src/basic/commands/utility/index.js
@@ -1,9 +1,11 @@
 import invite from './invite.js';
 import ping from './ping.js';
 import user from './user.js';
+import server from './server.js';
 
 export default function utility(client, commands) {
     invite(client, commands);
     ping(client, commands);
     user(client, commands);
+    server(client, commands);
 }
--- src/basic/commands/utility/server.js
+++ src/basic/commands/utility/server.js
@@ -11,23 +11,26 @@ export default function user (client, commands) {
             await interaction.deferReply({ephemeral: true});
             const server = interaction.guild;
             console.log(server);
-            const userInformation = new EmbedBuilder()
-                //.setColor(data.data.color)
-                .setTitle(interaction.user.globalName)
-                .setAuthor({ name: client.user.username + ' - USER', iconURL: client.user.displayAvatarURL({ dynamic: true, size: 1024 }) })
-                .setDescription(presence ? presence.activities.map(activity => activity.name).join(', ') : 'No current activities')
-                .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true, size: 1024 }))
+            let owner = await client.users.fetch(server.ownerId);
+            const serverInformation = new EmbedBuilder()
+                .setColor('#880000')
+                .setTitle(server.name)
+                .setAuthor({ name: client.user.username + ' - Server information', iconURL: client.user.displayAvatarURL({ dynamic: true, size: 1024 }) })
+                .setDescription('This server is owned by ' +  owner.globalName)
+                .setThumbnail(server.iconURL({ dynamic: true, size: 1024 }))
                 .addFields(
-                    { name: 'id', value: interaction.user.id, inline: true },
-                    { name: 'Bot', value: interaction.user.bot ? 'Yes' : 'No', inline: true },
-                    { name: 'Username', value: '@' + interaction.user.username, inline: true },
-                    { name: 'Status', value: presence ? presence.status : 'offline', inline: true },
-                    { name: 'Desktop', value: presence ? (presence.clientStatus.desktop ? presence.clientStatus.desktop : 'Offline') : 'Offline', inline: true },
-                    { name: 'Mobile', value: presence ? (presence.clientStatus.mobile ? presence.clientStatus.mobile : 'Offline') : 'Offline', inline: true }
-                )
+                    { name: 'id', value: server.id, inline: true },
+                    { name: 'Creation date', value: server.createdAt.toDateString(), inline: true },
+                    { name: 'Members', value: server.memberCount.toString(), inline: true },
+                    { name: 'Channels', value: server.channels.cache.size.toString(), inline: true },
+                    { name: 'Roles', value: server.roles.cache.size.toString(), inline: true },
+                    { name: 'MFA level', value: server.mfaLevel.toString(), inline: true },
+                    { name: 'Verification level', value: server.verificationLevel.toString(), inline: true },
+                    { name: 'Verified', value: server.verified ? 'Yes' : 'No', inline: true }
+                    )
                 .setTimestamp()
                 .setFooter({ text: interaction.user.globalName, iconURL: interaction.user.displayAvatarURL({ dynamic: true, size: 1024 }) });
-                interaction.editReply({ embeds: [userInformation], flags: MessageFlags.Ephemeral});
+                interaction.editReply({ embeds: [serverInformation], flags: MessageFlags.Ephemeral});
         }
     });
 }
