a7e07fd83556101505cde5239167cd10ee138b1b
Jimmy Béland-Bédard <jimmy.beland.bedard@hotmail.com>
2025-11-06 21:53:35 -0500

    Remove old module

--- _Old Modules/index.js
+++
@@ -1,8 +0,0 @@
-
-var basic = require("./Basic/index.js");
-var premium = require("./Premium/index.js");
-
-exports.loadAllModules = function(message, prefix) {
-    basic.loadCommands(message, prefix);
-    //premium.loadCommands(message, prefix);
-};
